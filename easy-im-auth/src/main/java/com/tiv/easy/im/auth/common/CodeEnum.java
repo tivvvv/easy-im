623b23a09b822a6c7422525e5b16ff3b846742fc
tivvvv <18612553935@163.com>
2025-08-24 21:17:04 +0800

    feat: 封装状态码枚举和全局响应结果.

---
+++ easy-im-auth/src/main/java/com/tiv/easy/im/auth/common/CodeEnum.java
@@ -0,0 +1,26 @@
+package com.tiv.easy.im.auth.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 状态码枚举
+ */
+@Getter
+@AllArgsConstructor
+public enum CodeEnum {
+
+    SUCCESS(200, "OK"),
+    REGISTER_ERROR(40001, "注册失败,用户已存在"),
+    CODE_ERROR(40002, "验证码错误"),
+    LOGIN_ERROR(40003, "登录失败,用户名或密码错误"),
+    NO_USER_ERROR(40004, "用户不存在"),
+
+    SYSTEM_ERROR(50000, "系统内部异常"),
+    UPDATE_AVATAR_ERROR(50001, "更新头像失败");
+
+    private final int code;
+
+    private final String msg;
+
+}
